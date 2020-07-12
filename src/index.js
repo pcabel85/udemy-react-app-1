883d87acfc88c170bad254d78d8c0e40ecbfb179
PetesaurusRex <pcabel85@gmail.com>
2020-07-12 00:57:26 -0700

    Using JS variable in JSX template

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let getButtonText = () => {
+    return "Click on me!"
+}
+
 // Create a React component 
 const App = () => {
-    return <div>Hi there!</div>
+    const styles = {
+        backgroundColor: 'blue',
+        color: 'white'
+    }
+
+    return (
+        <div>
+            <label for="name" className="label">Enter Name:</label>
+            <input id="name" type="text" />
+            <button style={styles}>{getButtonText()}</button>
+        </div>
+    )
 }
 
 //Take the React component and show it on the screen
