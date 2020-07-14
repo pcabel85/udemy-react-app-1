3d29eebf6fc68c7f4a56418f055b7675d5ec20c1
PetesaurusRex <pcabel85@gmail.com>
2020-07-14 16:57:48 -0700

    updated to className

--- app-2/src/ApprovalCard.js
+++ app-2/src/ApprovalCard.js
@@ -7,10 +7,10 @@ const ApprovalCard = (props) => {
                 <div className="content">
                     {props.children}
                 </div>
-                <div class="extra content">
-                    <div class="ui two buttons">
-                        <div class="ui basic green button">Approve</div>
-                        <div class="ui basic red button">Reject</div>
+                <div className="extra content">
+                    <div className="ui two buttons">
+                        <div className="ui basic green button">Approve</div>
+                        <div className="ui basic red button">Reject</div>
                     </div>
                 </div>
             </div>
