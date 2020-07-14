b86270f954abc621ac494fbc50a9f1d5b9cd1778
PetesaurusRex <pcabel85@gmail.com>
2020-07-14 00:26:26 -0700

    Added props to CommentDetail component

--- app-2/src/index.js
+++ app-2/src/index.js
@@ -7,10 +7,30 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments"> 
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
+            <CommentDetail 
+                author="Sam" 
+                time="1:30" 
+                post="Cool story bruh!" 
+                img={faker.image.avatar()} 
+            />
+            <CommentDetail 
+                author="Ben" 
+                time="2:30" 
+                post="Works on my machine!" 
+                img={faker.image.avatar()} 
+            />
+            <CommentDetail 
+                author="Jen" 
+                time="3:30" 
+                post="Let's do one thing..."
+                img={faker.image.avatar()} 
+            />
+            <CommentDetail 
+                author="Noms" 
+                time="4:30" 
+                post="Sarat Alalalallalalalalllaaa!"
+                img={faker.image.avatar()} 
+            />
         </div>
     );
 };
