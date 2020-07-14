7cd4faea48958bb82975229f14b6c8006830f955
Pete Cabel <pcabel@tirebuyer.com>
2020-07-14 15:58:38 -0700

    Added new project and components

---
+++ app-3/src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SeasonDisplay = (props) => {
+    return (
+        <div>Winter is Coming!</div>
+    )   
+}
+
+export default SeasonDisplay;
---
+++ app-3/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+
+const App = () => {
+    return (
+        <div>
+            Hi There!
+            <SeasonDisplay />
+        </div>
+    )
+}
+
+ ReactDOM.render(<App />, document.querySelector("#root"))
