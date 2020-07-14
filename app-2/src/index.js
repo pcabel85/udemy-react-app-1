c381833339c12868160af3ab0ddf57c70326ce30
PetesaurusRex <pcabel85@gmail.com>
2020-07-14 00:55:30 -0700

    Added ApprovalCard component and passed in CommentDetail component as props.children

--- app-2/src/index.js
+++ app-2/src/index.js
@@ -2,35 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
     return (
         <div className="ui container comments"> 
-            <CommentDetail 
-                author="Sam" 
-                time="1:30" 
-                post="Cool story bruh!" 
-                img={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author="Ben" 
-                time="2:30" 
-                post="Works on my machine!" 
-                img={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author="Jen" 
-                time="3:30" 
-                post="Let's do one thing..."
-                img={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author="Noms" 
-                time="4:30" 
-                post="Sarat Alalalallalalalalllaaa!"
-                img={faker.image.avatar()} 
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Ben" 
+                    time="2:30" 
+                    post="Works on my machine!" 
+                    img={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jen" 
+                    time="3:30" 
+                    post="Let's do one thing..."
+                    img={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Noms" 
+                    time="4:30" 
+                    post="Sarat Alalalallalalalalllaaa!"
+                    img={faker.image.avatar()} 
+                />
+            </ApprovalCard>
         </div>
     );
 };
