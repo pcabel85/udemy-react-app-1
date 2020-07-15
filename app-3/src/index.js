7c5834c3acdfb916949eaf07dc7d5527fbde047d
PetesaurusRex <pcabel85@gmail.com>
2020-07-14 23:39:54 -0700

    Converted App from a functional component to a Class based component

--- app-3/src/index.js
+++ app-3/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-    return (
-        <div>
-            Hi There!
-            <SeasonDisplay />
-        </div>
-    )
+//Converted App from a functional component to a Class based component to
+//take advantage of React.Component's built in methods.
+class App extends React.Component {
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)  
+        )
+        return <div>Latitude</div>
+    }
 }
 
  ReactDOM.render(<App />, document.querySelector("#root"))
