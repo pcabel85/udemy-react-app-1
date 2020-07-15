84daf767e69848627ebc465b71e6eb7f2144ca90
PetesaurusRex <pcabel85@gmail.com>
2020-07-15 00:15:22 -0700

    Added new constructor function and state object

--- app-3/src/index.js
+++ app-3/src/index.js
@@ -5,12 +5,25 @@ import SeasonDisplay from "./SeasonDisplay";
 //Converted App from a functional component to a Class based component to
 //take advantage of React.Component's built in methods.
 class App extends React.Component {
-    render(){
+    constructor(props) { //First function called when component is init'd 
+        super(props);    //Passes in parent's (React.Component) constructor function   
+
+        this.state = {
+            lat: null
+        };
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            position => {
+                // Called setState to update our component!!!
+                this.setState({ lat: position.coords.latitude })
+            },
             (err) => console.log(err)  
         )
-        return <div>Latitude</div>
+    }
+
+    //We always have to define render()!!!
+    render(){
+        return <div>Latitude: {this.state.lat}</div>
     }
 }
 
