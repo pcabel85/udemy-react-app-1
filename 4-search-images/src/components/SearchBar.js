0acb1149ce239e364d6670cef0bfe0be02990454
PetesaurusRex <pcabel85@gmail.com>
2020-07-29 00:42:24 -0700

    Too tired to make commit msg

--- 4-search-images/src/components/SearchBar.js
+++ 4-search-images/src/components/SearchBar.js
@@ -5,10 +5,16 @@ class SearchBar extends React.Component {
         term: ''
     };
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        
+       this.props.onSubmit(this.state.term);
+    }
+
     render(){
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <label>Image Search</label>
                     {/* Dont add () when function is acting as a callback handler for user interaction, such as onChange */}
                     <input 
